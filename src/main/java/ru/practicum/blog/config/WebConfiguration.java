25d2cad06a4a7a607e680f8f1563ef2bdb8564fb
oakhmetov <oakhmetov@utracesolutions.com>
2025-01-18 12:23:54 +0500

    Настройка hibernate/jpa

--- src/main/java/ru/practicum/blog/config/WebConfiguration.java
+++ src/main/java/ru/practicum/blog/config/WebConfiguration.java
@@ -1,5 +1,7 @@
 package ru.practicum.blog.config;
 
+import java.util.Properties;
+
 import javax.sql.DataSource;
 
 import lombok.RequiredArgsConstructor;
@@ -13,8 +15,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
@@ -22,7 +26,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ComponentScan(basePackages = "ru.practicum.blog")
 @PropertySource("classpath:application.properties")
 @RequiredArgsConstructor
-//@EnableJpaRepositories("ru.practicum.blog.repository")
+@EnableJpaRepositories("ru.practicum.blog.repository")
 public class WebConfiguration {
 
     private final Environment env;
@@ -43,7 +47,30 @@ public class WebConfiguration {
     }
 
     @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        final LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setPackagesToScan("ru.practicum.blog");
+
+        final HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        em.setJpaProperties(hibernateProperties());
+
+        return em;
+    }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", env.getRequiredProperty("hibernate.dialect"));
+        properties.put("hibernate.show_sql", env.getRequiredProperty("hibernate.show_sql"));
+
+        return properties;
+    }
+
+    @Bean
     public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
         return new DataSourceTransactionManager(dataSource);
     }
     @Bean
