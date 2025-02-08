9d815d9bda85e8b4f75f2ce87d1d91a59c2752e7
oakhmetov <oakhmetov@utracesolutions.com>
2025-02-08 14:52:07 +0500

    Фикс конфигурации интеграционного теста

--- src/main/resources/db/migration/V6__alter-comment-add-created_at.sql
+++ src/main/resources/db/migration/V6__alter-comment-add-created_at.sql
@@ -1,6 +1,6 @@
-ALTER TABLE comment
+ALTER TABLE "comment"
     ADD COLUMN created_at timestamptz DEFAULT CURRENT_TIMESTAMP NOT NULL;
 
 CREATE INDEX idx_post_created_at ON post(created_at);
 
-CREATE INDEX idx_comment_created_at ON comment(created_at);
+CREATE INDEX idx_comment_created_at ON "comment"(created_at);
--- src/test/java/ru/practicum/post/PostIntegrationTest.java
+++ src/test/java/ru/practicum/post/PostIntegrationTest.java
@@ -7,55 +7,52 @@ import static org.hamcrest.Matchers.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 import ru.practicum.blog.PracticumBlogBootApplication;
 import ru.practicum.blog.model.Post;
 import ru.practicum.blog.repository.PostRepository;
 import ru.practicum.config.EnablePostgresTest;
 
 @SpringBootTest(classes = PracticumBlogBootApplication.class)
+@AutoConfigureMockMvc
 @EnablePostgresTest
 @ActiveProfiles("test")
 public class PostIntegrationTest {
 
     @Autowired
-    private WebApplicationContext wac;
-
-    @Autowired
     private PostRepository postRepository;
 
+    @Autowired
     private MockMvc mockMvc;
 
     @BeforeEach
     public void setUp() {
-        // Инициализация MockMvc с контекстом приложения
-        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
-    }
-
-    private void createPost(String title) {
-        Post post = new Post();
-        post.setTitle(title);
-        postRepository.save(post);
+        postRepository.deleteAll();
     }
 
     @Test
     public void allPost_ShouldReturnPostsPage() throws Exception {
-        createPost("Test post");
+        createPost();
         mockMvc.perform(get("/post/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("posts/index"))
                 .andExpect(model().attributeExists("posts"))
                 .andExpect(model().attribute("posts", hasItem(
                         anyOf(
-                                hasProperty("title", is("Test post"))
+                                hasProperty("title", is("Test post title"))
                         )
                 )))
                 .andExpect(model().attribute("currentPage", 0))
                 .andExpect(model().attribute("totalPages", 1))
                 .andExpect(model().attribute("size", 10));
+    }
+
+    private void createPost() {
+        Post post = new Post();
+        post.setTitle("Test post title");
+        postRepository.save(post);
     }
 }
